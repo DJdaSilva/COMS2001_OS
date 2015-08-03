89aeb8546ea3431ac6068a6746f775cba976ed59
Daniel <daniel@ubuntu.(none)>
2015-08-03 18:01:48 +0200

    HW0 completed.

--- hw0/main.c
+++ hw0/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <limits.h>
+#include <unistd.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %d\n",  0);
-    printf("process limit: %d\n", 0);
-    printf("max file descriptors: %d\n", 0);
+    	struct rlimit lim;
+	getrlimit(RLIMIT_STACK, &lim);
+	printf("stack size: %d\n",  (int)lim.rlim_cur);
+	getrlimit(RLIMIT_DATA, &lim);
+   	printf("process limit: %d\n", (int)sysconf(_SC_CHILD_MAX));
+	getrlimit(RLIMIT_NOFILE, &lim);
+    	printf("max file descriptors: %d\n", (int)lim.rlim_cur);
 }
--- hw0/wc.c
+++ hw0/wc.c
@@ -3,14 +3,63 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void wc ( FILE *infile, char *inname);
+int totalChars = 0;
+int totalWords = 0;
+int totalLines = 0;
 
-int
-main (int argc, char *argv[]) {
-    
+void wc( FILE *infile, char *inname) {
+	int chars = 0;
+	int words = 0;
+	int lines = 0;
+	bool wordflag = false;
+	char curr;
+
+	if ( (infile = fopen(inname, "r")) == NULL) {
+		printf("File not found!");
+	}
+	
+	while (!feof(infile)) {
+		curr = fgetc(infile);
+
+		if ( (curr != '\n') && (curr != '\t') ) {
+			chars++;
+			if (wordflag == true) {
+				words++;
+				wordflag = false;
+			}
+		}
+
+		if ((curr == ' ') || (curr == '\n') || (curr == '\t')) {
+			wordflag = true;
+			if (curr == '\n') {
+				lines++;
+			}
+		}
+	
+	}
+	chars--; //Not 100% sure, likely a whitespace/blankspace char I've forgotten about
+	
+	totalChars += chars;
+	totalWords += words;
+	totalLines += lines; 
+	printf("%s:\t%d\t\t%d\t%d\n", inname, chars, words, lines);
+	
 }
 
-void
-wc( FILE *infile, char *inname) {
-    
+int main(int argc, char *argv[]) {
+
+	FILE *File;
+	int k;
+
+	printf("\nFile name\tCharacters\tWords\tLines\n");
+	
+	for (k = 1; k < argc; k++) {
+		wc(File, argv[k]);
+	}
+
+	printf("Totals:\t\t%d\t\t%d\t%d\n", totalChars, totalWords, totalLines);
+	
+	
 }
+
+
